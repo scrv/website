a0ea6284ed3383efa235859600d5eed725816d1c
Sebastian Carrera <scrv.201@outlook.com>
2024-08-24 00:36:34 +0900

    feat: configure mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
+import createMDX from "@next/mdx";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: process.env.BASE_PATH,
+  output: "export",
+  basePath: process.env.BASE_PATH,
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
