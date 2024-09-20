e3b8590b73730722c854de4fd5e6d52fa1b72fb5
Sebastian Carrera <scrv.201@outlook.com>
2024-09-20 21:32:18 +0900

    refactor: formatted code

--- src/app/engineering-daybook/[slug]/page.tsx
+++ src/app/engineering-daybook/[slug]/page.tsx
@@ -1,24 +1,28 @@
-import { getEntries } from '@/app/utils/engineering-daybook'
-import { notFound } from 'next/navigation'
-import { MDXRemote } from 'next-mdx-remote/rsc'
+import { getEntries } from "@/app/utils/engineering-daybook";
+import { notFound } from "next/navigation";
+import { MDXRemote } from "next-mdx-remote/rsc";
 
 export function generateStaticParams() {
-    let entries = getEntries()
+  let entries = getEntries();
 
-    return entries.map((entry) => ({
-        slug: entry.slug,
-    }))
+  return entries.map((entry) => ({
+    slug: entry.slug,
+  }));
 }
 
 export default function EngineeringDaybookEntry({ params }: any) {
-    let entry = getEntries().find((post) => post.slug === params.slug)
+  let entry = getEntries().find((post) => post.slug === params.slug);
 
-    if (!entry) {
-        notFound()
-    }
+  if (!entry) {
+    notFound();
+  }
 
-    return <>
-    {entry.metadata.tags.map((tag: string) => <span key={tag}>{tag}</span>)}
-       <MDXRemote source={entry.content} />
+  return (
+    <>
+      {entry.metadata.tags.map((tag: string) => (
+        <span key={tag}>{tag}</span>
+      ))}
+      <MDXRemote source={entry.content} />
     </>
+  );
 }
--- src/app/engineering-daybook/page.tsx
+++ src/app/engineering-daybook/page.tsx
@@ -1,11 +1,15 @@
-import { getEntries } from '@/app/utils/engineering-daybook'
-import Link from 'next/link'
+import { getEntries } from "@/app/utils/engineering-daybook";
+import Link from "next/link";
 
 export default function EngineeringDaybook() {
-    let entries = getEntries()
-    return <>
-    {
-        entries.map(el => (<Link key={el.slug} href={`engineering-daybook/${el.slug}`}>{el.slug}</Link>))
-    }
+  let entries = getEntries();
+  return (
+    <>
+      {entries.map((el) => (
+        <Link key={el.slug} href={`engineering-daybook/${el.slug}`}>
+          {el.slug}
+        </Link>
+      ))}
     </>
+  );
 }
